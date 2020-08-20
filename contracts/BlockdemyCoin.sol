a9ec383507c541958666e87e61756ea39851acbb
ernestognw <ernestognw@gmail.com>
2020-08-19 19:50:58 -0500

    Adding coin smart contract

---
+++ contracts/BlockdemyCoin.sol
@@ -0,0 +1,31 @@
+pragma solidity >=0.4.21 <0.7.0;
+
+// imports
+
+/**
+ * Contrato para almacenar los balances del BlockdemyCoin
+ *
+ * // Balance del token
+ * Address -> Cantidad
+ *
+ */
+contract BlockdemyCoin {
+    // Variables
+    mapping(address => uint256) balances;
+
+    // Constructor
+    constructor() public {
+        balances[msg.sender] = 1000;
+    }
+
+    // Métodos
+    function send(address receiver, uint256 amount) public {
+        require(balances[msg.sender] > amount);
+        balances[msg.sender] -= amount;
+        balances[receiver] += amount;
+    }
+
+    function getBalance() public view returns (uint256) {
+        return balances[msg.sender];
+    }
+}
