1d279414f4a9b8ff6c732b0a04f5625527e9080c
ernestognw <ernestognw@gmail.com>
2020-08-19 20:39:56 -0500

    Add tests

---
+++ test/TestBlockdemyCoin.sol
@@ -0,0 +1,17 @@
+pragma solidity >=0.4.21 <0.7.0;
+
+import "truffle/Assert.sol";
+import "truffle/DeployedAddresses.sol";
+import "../contracts/BlockdemyCoin.sol";
+
+contract TestBlockdemyCoin {
+
+  function testInitialBalanceOfOwner() public {
+    BlockdemyCoin coin = BlockdemyCoin(DeployedAddress.BlockdemyCoin());
+
+    uint expectedBalance = 1000;
+
+    Assert.equal(meta.getBalance(tx.origin), expected, 'First account does not have a 1000 coins')
+  };
+
+}
---
+++ test/blockdemycoin.js
@@ -0,0 +1,18 @@
+const _deploy_contracts = require('../migrations/2_deploy_contracts');
+
+const BlockdemyCoin = artifacts.require('BlockdemyCoin');
+
+contract('Check Blockdemy Coin', () => {
+  it('Should put 1000 in the first account', async () => {
+    const coin = await BlockdemyCoin.deployed();
+    const balance = await coin.getBalance(accounts[0]);
+
+    const balanceNumber = balance.toNumber();
+
+    assert.equal(
+      1000,
+      balanceNumber,
+      'First account does not have a 1000 coins'
+    );
+  });
+});
