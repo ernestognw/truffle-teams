5c63d40724e938ecc5fd85f9e18b7ca8c892d11a
ernestognw <ernestognw@gmail.com>
2020-08-19 20:43:27 -0500

    fix tests

--- test/TestBlockdemyCoin.sol
+++ test/TestBlockdemyCoin.sol
@@ -5,13 +5,15 @@ import "truffle/DeployedAddresses.sol";
 import "../contracts/BlockdemyCoin.sol";
 
 contract TestBlockdemyCoin {
+    function testInitialBalanceOfOwner() public {
+        BlockdemyCoin coin = BlockdemyCoin(DeployedAddress.BlockdemyCoin());
 
-  function testInitialBalanceOfOwner() public {
-    BlockdemyCoin coin = BlockdemyCoin(DeployedAddress.BlockdemyCoin());
+        uint256 expectedBalance = 1000;
 
-    uint expectedBalance = 1000;
-
-    Assert.equal(meta.getBalance(tx.origin), expected, 'First account does not have a 1000 coins')
-  };
-
+        Assert.equal(
+            meta.getBalance(tx.origin),
+            expected,
+            "First account does not have a 1000 coins"
+        );
+    }
 }
--- test/blockdemycoin.js
+++ test/blockdemycoin.js
@@ -1,5 +1,3 @@
-const _deploy_contracts = require('../migrations/2_deploy_contracts');
-
 const BlockdemyCoin = artifacts.require('BlockdemyCoin');
 
 contract('Check Blockdemy Coin', () => {
